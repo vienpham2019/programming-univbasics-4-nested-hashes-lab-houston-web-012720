a9a6e2e332ec89a4fb2b3156b0d3d1ef2cab1d4e
vien pham <vienpham2019@gmail.com>
2019-12-10 04:04:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay] = {:alans_new_info => "GUI" , :languages =>["Smalltalk","LISP"]
-     return programmer_hash
+  programmer_hash[:alan_kay] = {:alans_new_info => "GUI" , :languages =>["Smalltalk","LISP"]
+  return programmer_hash
 end
 
 def adding_to_dennis
